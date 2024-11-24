428a496f14b92a99fdbdb8b490426c52bf9d252d
yuhoyeong <168501145+yuhoyeong@users.noreply.github.com>
2024-11-25 03:17:42 +0900

    네비바에서 헤더분리(app.js,header.js,navbar.js 수정)

--- src/App.test.js
+++
@@ -1,7 +0,0 @@
-/* index.css 파일 */
-body, html {
-    margin: 0;
-    padding: 0;
-    overflow: hidden; /* 스크롤 방지 */
-  }
-  
--- src/components/Foodlist.js
+++
@@ -1,8 +0,0 @@
-// src/components/Footer.js
-import React from 'react';
-
-function Footer() {
-  return <footer><p>Website Footer</p></footer>;
-}
-
-export default Footer;
